7d6817225a6270758fa175b7f657627d8d8be99e
Quinteto6969_2C <anabeatrizad6@gmail.com>
2025-08-21 02:12:31 +0000

    Código Final

---
+++ perguntas.js
@@ -0,0 +1,37 @@
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Independência do Brasil?',
+    '1822'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca'
+)
+
+criaCartao(
+    'História',
+    'Qual país europeu colonizou o Brasil?',
+    'Portugal'
+)
+
+criaCartao(
+    'História',
+    'Em que ano aconteceu a Revolução Francesa?',
+    '1789.'
+
+)
+
+criaCartao(
+    'História',
+    'Quem foi o líder militar francês que se tornou imperador?',
+    'Napoleão Bonaparte'
+)
+
+criaCartao(
+    'História',
+    'Qual muro foi derrubado em 1989, simbolizando o fim da Guerra Fria?',
+    'Muro de Berlim'
+
+)
